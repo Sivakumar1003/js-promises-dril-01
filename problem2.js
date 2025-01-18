d111a673159b3d7697943200a2026a62f35e3c00
Sivakumar N <shivask94423@gmail.com>
2025-01-18 17:52:04 +0530

    modified function by promises method.

--- problem2.js
+++ problem2.js
@@ -1,121 +1,134 @@
-const fileSystem = require('fs').promises;
+const fileSystem = require('fs');
 
 //  function to read the file and return the data in it.
 function readFile(path) {
-    return fileSystem.readFile(path, 'utf-8')
-        .then((data) => {
-            return data;
-        })
-        .catch((error) => {
-            console.error("not able to read the file.");
-            return error;
+    //  returning a promises.
+    return new Promise((resolve, reject) => {
+        fileSystem.readFile(path, 'utf-8', (error, data) => {
+            if (error) {
+                reject("Not able to read the file.");
+            } else {
+                resolve(data);
+            }
         });
+    });
 }
 
 //  function to convert Upper  case and cerate ne file for it.
 function convertUpperCase(data) {
     let UpperCaseData = data.toUpperCase();
-    let path = './upper-case-file.txt';
 
-    // creating new file.
-    return fileSystem.writeFile(path, UpperCaseData)
-        .then(() => {
-            return addFileName(path);
-        }).then(() => {
-            return path;
-        })
-        .catch((error) => {
-            console.error("err.");
-            return error;
+    return new Promise((resolve, reject) => {
+
+        // creating new file.
+        fileSystem.writeFile('./upper-case-file.txt', UpperCaseData, (error) => {
+            if (error) {
+                reject("Not able to create upper-case-file.txt.");
+            } else {
+                resolve(addFileName('./upper-case-file.txt'));
+            }
         });
+    });
 
 }
 
 // function to  convert lower case and split into new file.
 function convertLowerCaseAndSplit(path) {
-    let newPath = './lower-case-file.txt';
 
-    // reading the previouds file by path.
-    return fileSystem.readFile(path, 'utf-8')
-        .then((data) => {
-            let lowerCase = data.split('.')
-                .reduce((file, sentence) => {
-                    sentence = sentence.trim().toLowerCase();
-                    return file + sentence + '\n';
-                }, "");
+    return new Promise((resolve, reject) => {
+        // reading the previouds file by path.
+        fileSystem.readFile(path, 'utf-8', (error, UpperCaseData) => {
+            if (error) {
+                reject("Not ble to read the Upper case file.");
+            } else {
+                let lowerCaseData = UpperCaseData.split(".")
+                    .reduce((file, sentence) => {
+                        sentence = sentence.trim().toLowerCase();
+                        return file + sentence + '\n';
+                    }, "");
 
-            //  create new file for store the converted lower case file.
-            return fileSystem.writeFile(newPath, lowerCase)
-        })
-        .then(() => {
-            return addFileName(newPath);
-        })
-        .then(() => {
-            return newPath;
-        })
-        .catch((error) => {
-            console.error("not able to convert to lower case and split to new line.");
-            return error;
-        })
+                    // creating new file.
+                    fileSystem.writeFile('./lower-case-file.txt', lowerCaseData, (error) => {
+                        if (error) {
+                            reject("Not able to create new file Lower case file.");
+                        } else {
+                        resolve(addFileName('./lower-case-file.txt'));
+                    }
+                });
+            }
+        });
+    });
 }
 
 //  function to sort the conten inside the file.
 function sortFile(path) {
-    let newPath = './sort-sentence-file.txt';
+    
+    return new Promise((resolve, reject) => {
 
-    //  create new file for it.
-    return fileSystem.readFile(path, 'utf-8')
-        .then((data) => {
-            sortSentence = data.split('\n')
-                .sort().reduce((file, line) => {
+        // reading the previouds file by path.
+        fileSystem.readFile(path, 'utf-8', (error, lowerCaseData) => {
+            if (error) {
+                reject("Not ble to read the Lower case file.");
+            } else {
+                sortSentence = lowerCaseData.split('\n').sort()
+                .reduce((file, line) => {
                     return file + line + "\n";
                 }, "");
 
-            return fileSystem.writeFile(newPath, sortSentence)
-        })
-        .then(() => {
-            return addFileName(newPath)
-        })
-        .then(() => {
-            return newPath;
-        })
-        .catch((error) => {
-            console.error(`not able to sort the sentence in ${path} file.`);
-            return error;
-        })
+                //  create new file for it. 
+                fileSystem.writeFile('./sort-sentence-file.txt', sortSentence, (error) => {
+                    if (error) {
+                        reject("Not able to create new file Lower case file.")
+                    } else {
+                        resolve(addFileName('./sort-sentence-file.txt'))
+                    }
+                });
+            }
+        });
+    });
 }
 
 //  function to delete all the new file created.
 function deleteFiles(path) {
-    return fileSystem.readFile(path, 'utf-8')
-        .then((files) => {
-            files = files.split('\n')
+
+    return new Promise((resolve, reject) => {
+        // reading the previouds file by path.
+        fileSystem.readFile(path, 'utf-8', (error, filesData) => {
+            if(error) {
+                reject("Cannot able to read the fileName.txt file.")
+            } else {
+                filesData = filesData.split('\n')
                 .filter((lines) => {
                     return lines !== "";
                 });
 
-            //  delete all files.
-            for (let file of files) {
-                fileSystem.unlink(file);
+                //  deleting all file in fileName.txt
+                for(let fileName of filesData) {
+                    fileSystem.unlink(fileName, (error) => {
+                        if(error) {
+                            reject(`Cannot able to delete the file ${fileName.slice(2)}`);
+                        } 
+                    })
+                }
+                resolve("All files are deleted.");
             }
         })
-        .catch((error) => {
-            console.error("not able to delete the files.");
-            return error;
-        })
+    });
 }
 
 //  function to add file name for new file in fileName.txt file.
 function addFileName(path) {
-    return fileSystem.writeFile('./fileName.txt', path + '\n', { flag: 'a' })
-        .then(() => {
-            console.log(`${path.slice(2)} file added.`);
-            return path;
+    return new Promise((resolve, reject) => {
+        fileSystem.writeFile('./fileName.txt', path + '\n', { flag: 'a' }, (error) => {
+            if (error) {
+                reject(`not able to add ${path} in fileName.txt.`);
+            } else {
+                console.log(`${path.slice(2)} file added.`);
+                resolve(path);
+            }
         })
-        .catch((error) => {
-            console.log(`not able to create ${path.slice(2)} file`);
-            return error;
-        })
+
+    })
 }
 
 module.exports = { readFile, convertUpperCase, convertLowerCaseAndSplit, sortFile, deleteFiles };
