2b8e82c4344dbe35cf2506cc3c0f3ba640004851
Sivakumar N <shivask94423@gmail.com>
2025-01-18 16:00:02 +0530

    function modified by Promises.

--- problem1.js
+++ problem1.js
@@ -1,33 +1,39 @@
-const fileSystem = require('fs').promises;
+const fileSystem = require('fs');
 
 //  function to create and delete files simultaneously.
-function createAndDeleteFiles(fileCount, data) {
+function createFiles(fileCount, data) {
 
-    //  create a directory.
-    fileSystem.mkdir('./DemoFolder', { recursive: true })
-        .then((res) => {
-            console.log("folder created.");
-            let fileCreateResult = [];
-            for (let index = 1; index <= fileCount; index++) {
-                fileCreateResult.push(fileSystem.writeFile(`./DemoFolder/file_${index}.json`, JSON.stringify(data)));
+    return new Promise((resolve, reject) => {
+        //  create a directory.
+        fileSystem.mkdir('./DemoFolder', { recursive: true }, ((error) => {
+            if (error) {
+                reject("Not able to create new folder");
+            } else {
+                console.log("folder created.");
+                for (let index = 1; index <= fileCount; index++) {
+                    fileSystem.writeFile(`./DemoFolder/file_${index}.json`, JSON.stringify(data), ((error) => {
+                        if (error) {
+                            reject(`Not able to create file_${index}.json`);
+                        }
+                    }));
+                }
+                resolve("all files are created.")
             }
-            Promise.all(fileCreateResult);
-        })
-        .then(() => {
-            console.log(`${fileCount} files created.`);
-
-            let fileDeleteResult = [];
-            for (let index = 1; index <= fileCount; index++) {
-                fileDeleteResult.push(fileSystem.unlink(`./DemoFolder/file_${index}.json`));
-            }
-
-            //  check all the file are created else all will pass the error.
-            Promise.all(fileDeleteResult);
-        })
-        .catch((error) => {
-            console.log("not able to create files.");
-            console.log(error);
-        });
+        }))
+    });
 }
 
-module.exports = createAndDeleteFiles;
+function deleteFiles(fileCount) {
+    return new Promise((resolve, reject) => {
+        for (let index = 1; index <= fileCount; index++) {
+            fileSystem.unlink(`./DemoFolder/file_${index}.json`, (error) => {
+                if(error) {
+                    reject(`not able to delete file_${index}.json file.`)
+                }
+            });
+        }
+        resolve("all files are deleted.")
+    })
+}
+
+module.exports = { createFiles, deleteFiles };
