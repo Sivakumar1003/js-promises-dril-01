5b62f9823d7c4a16f0ccd3f51be2ad421a0f3dec
Sivakumar N <shivask94423@gmail.com>
2025-01-18 18:40:01 +0530

    test file for calling create and delete file.

--- Test/testProblem1.js
+++ Test/testProblem1.js
@@ -5,23 +5,6 @@
         2. Delete those files simultaneously 
 */
 
-const {createFiles, deleteFiles} = require('../problem1');
+const {testResult} = require('../problem1');
 
-let fileCount = 4;
-let data = {
-    'name': "sivakumar", 
-    'age': 22
-}
-
-// calling the function.
-createFiles(fileCount, data)
-.then((result) => {
-    console.log(result);
-    return deleteFiles(fileCount)
-})
-.then((deleteResult)=> {
-    console.log(deleteResult);
-})
-.catch((error) => {
-    console.log(error);
-})
+testResult();
